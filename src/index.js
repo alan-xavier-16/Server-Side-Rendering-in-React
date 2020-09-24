eaeaa7d32581ae17fdf190f2d180b3f771124744
Alan Xavier <alan_xvr@hotmail.com>
2020-09-24 18:34:32 -0400

    Redux setup for SSR 1. src/client/client.js   - Setup 'client-side' redux     - import BrowserRouter from "react-router-dom"     - import createStore, applyMiddleware from "redux"     - import thunk from "redux-thunk"     - import Provider from "react-redux"   - Create a Redux store   - Pass store using Provider 2. src/helpers/createStore.js   - Create helper to create a Redux store for 'server-side' 3. src/index.js   - Create Redux 'store' using 'createStore helper' and pass to 'renderer helper' 4. src/helpers/renderer.js   - import Provider from "react-redux"   - Pass store using Provider 5. src/client/actions/index.js   - Basic actions setup in Redux 6. src/client/reducers/usersReducer.js   - Basic reducer setup in Redux 7. src/client/reducers/index.js   - Combine reducers for React 8. src/client/components/UsersList.component.js   - Renders a list of 'users' using Redux actions 9. src/client/Routes.js   - Add Route to UsersList

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+import "babel-polyfill";
 import express from "express";
 import renderer from "./helpers/renderer";
+import createReduxStore from "./helpers/createStore";
+
 const app = express();
 
 // EXPRESS TO MAKE THIS DIRECTORY AVAILABLE TO BROWSERS
 app.use(express.static("public"));
 
 app.get("*", (req, res) => {
-  res.send(renderer(req));
+  const store = createReduxStore();
+  res.send(renderer(req, store));
 });
 
 app.listen(3000, () => {
