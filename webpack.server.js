e382984581bb83ce8a5e9da19fce216c69f67a91
Alan Xavier <alan_xvr@hotmail.com>
2020-09-24 16:10:29 -0400

    Use 'webpack-node-externals' to reduce 'node_modules' for server

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const merge = require("webpack-merge");
 const baseConfig = require("./webpack.base");
+const webpackNodeExternals = require("webpack-node-externals");
 
 const config = {
   // Inform webpack to build a bundle for nodeJS, rather than for the browser
@@ -12,6 +13,7 @@ const config = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build"),
   },
+  externals: [webpackNodeExternals()],
 };
 
 module.exports = merge(baseConfig, config);
