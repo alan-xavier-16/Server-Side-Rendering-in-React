3b4052cc67965b7eca9a1e0e190d3d614e2663e4
Alan Xavier <alan_xvr@hotmail.com>
2020-09-24 19:23:13 -0400

    Use 'react-router-config' library for SSR 1. src/client/Routes.js   - Convert 'Routes' component to return an array of objects 2. src/client/client.js && src/helpers/renderer.js   - Use 'react-router-config' renderRoutes function to load 'Routes'

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,13 +1,8 @@
 import React from "react";
-import { Route } from "react-router-dom";
 import Home from "./components/Home.component";
 import UsersList from "./components/UsersList.component";
 
-export default () => {
-  return (
-    <div>
-      <Route exact path="/" component={Home} />
-      <Route path="/users" component={UsersList} />
-    </div>
-  );
-};
+export default [
+  { path: "/", component: Home, exact: true },
+  { path: "/users", component: UsersList },
+];
